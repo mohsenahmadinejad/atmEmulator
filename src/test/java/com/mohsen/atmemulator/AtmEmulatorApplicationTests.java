2e5929ed2e41167fcae9c963ab028b04d0ddfb97
mahmadinejad <ahmadi.mohsen.s@gmail.com>
2022-11-10 09:37:44 +0330

    first commit/init commit

---
+++ src/test/java/com/mohsen/atmemulator/AtmEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsen.atmemulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmEmulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
